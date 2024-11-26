22c3cd533ac00e629c41959cc654ff05b2e993e0
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2024-11-26 17:03:49 +0300

    tets

---
+++ resources/js/Pages/Home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- <HomeSearch /> -->
+  <div id="main">
+    <section class="banner-1 section-first">
+      <div class="container">
+        <div
+          class="row justify-content-center align-items-center"
+          style="min-height: 90vh"
+        >
+          <h2>Under Meintainance</h2>
+          <p>Please check later...</p>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
---
+++ resources/js/Shared/Layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2>Website Under Meintainance</h2>
+  <p>Please check later</p>
+  <!-- <Nav />
+  <Flash />
+
+  <slot />
+  <Footer /> -->
+</template>
+
+<script setup>
+// import Nav from "./Nav.vue";
+// import Footer from "./Footer.vue";
+// import Flash from "./Flash.vue";
+</script>
